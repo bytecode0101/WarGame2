25fa2acd1c535ba7a839e6a9650f6414ddc27a7b
ovidiu.szanto <bytecode0101@yahoo.com>
2016-05-14 13:05:46 +0300

    .

---
+++ Curs18/Unity/TilePathGame/Assets/Battleship/Coordinate.cs
@@ -0,0 +1,12 @@
+﻿public class Coordinate
+{
+    /// <summary>
+    /// The Ox coordinate
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// The Oy coordinate
+    /// </summary>
+    public int Y { get; set; }
+}
